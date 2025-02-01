f0341433b4be39d86debd107fe8b2eeacf707715
Md-Alqma <mdalqma7@gmail.com>
2025-02-01 19:24:40 +0530

    admin login route and admin authentication middleware created

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const PORT = process.env.PORT || 3000;
 
 let ADMINS = [];
 
+const adminAuthentication = (req, res, next) => {
+  const { username, password } = req.headers;
+  console.log("inside authenticatoin");
+  const admin = ADMINS.find(
+    (a) => a.username === username && a.password === password
+  );
+  if (admin) {
+    next();
+  }
+  res.status(403).json({ message: "Admin authentication failed" });
+};
+
 app.get("/", (req, res) => {
   res.send("Welcome to Courses for You");
 });
@@ -17,9 +29,13 @@ app.post("/admin/signup", (req, res) => {
   if (adminExists) {
     res.status(401).json({ message: "Admin already exists" });
   } else {
-    ADMINS.push(adminExists);
+    ADMINS.push(admin);
     res.status(200).json({ message: "Admin created successfully" });
   }
+});
+
+app.post("/admin/login", adminAuthentication, (req, res) => {
+  res.status(200).json({ message: "Admin login successful" });
 });
 
 app.listen(PORT, () => {
