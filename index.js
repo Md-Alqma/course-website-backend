d79df8ebd1e544f53d1c60c89ad443899819920d
Md-Alqma <mdalqma7@gmail.com>
2025-02-01 19:15:39 +0530

    signup route created

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+let ADMINS = [];
+
 app.get("/", (req, res) => {
   res.send("Welcome to Courses for You");
+});
+
+app.post("/admin/signup", (req, res) => {
+  const admin = req.body;
+  const adminExists = ADMINS.find((a) => a.username === admin.username);
+  if (adminExists) {
+    res.status(401).json({ message: "Admin already exists" });
+  } else {
+    ADMINS.push(adminExists);
+    res.status(200).json({ message: "Admin created successfully" });
+  }
 });
 
 app.listen(PORT, () => {
